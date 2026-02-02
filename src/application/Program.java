0964b247f5854384998260381c98ab6d748da6b8
Pedro Henrique <ph10rabelo@gmail.com>
2026-02-02 09:56:08 -0300

    Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
-
+import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Position pos = new Position(1,2);
+		System.out.println(pos);
 	}
 
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,28 @@
+package boardgame;
+
+public class Position {
+	private int row;
+	private int colunm;
+	public Position(int row, int colunm) {
+		super();
+		this.row = row;
+		this.colunm = colunm;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColunm() {
+		return colunm;
+	}
+	public void setColunm(int colunm) {
+		this.colunm = colunm;
+	}
+	
+	@Override
+	public String toString() {
+		return row+", "+colunm;
+	}
+}
