60c8ae142d51257f8c74ba389b51f23ff013389d
Pedro Henrique <ph10rabelo@gmail.com>
2026-02-02 16:55:24 -0300

    board vision

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,11 @@
 package application;
 import boardgame.Position;
+import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		Position pos = new Position(1,2);
-		System.out.println(pos);
+		ChessMatch match = new ChessMatch();
+		UI.printBoard(match.getPieces());
 	}
 
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,32 @@
 package boardgame;
 
 public class Board {
-
+	private int rows;
+	private int colunms;
+	private Piece[][] pieces;
+	public Board(int row, int colunm) {
+		rows = row;
+	    colunms = colunm;
+		pieces = new Piece[row][colunm];
+	}
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getColunms() {
+		return colunms;
+	}
+	public void setColunms(int colunms) {
+		this.colunms = colunms;
+	}
+	
+	public Piece piece(int row, int colunm) {
+		return pieces[row][colunm];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColunm()];
+	}
 }
